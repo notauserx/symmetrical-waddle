f19e6efddcc6c2eae45ed5481c3fdf4124307e71
notauserx <notauserx@users.noreply.github.com>
2022-11-03 18:38:44 +0600

    wip with initial migration

--- src/README.md
+++ src/README.md
@@ -1,1 +1,7 @@
 docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=yourStrong(!)Password" -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
+
+# Migrations are set up to be on a seperate project. Use 
+
+```shell
+dotnet ef migrations add InitialCreate --project ..\..\Data\Context.Migrations
+```
---
+++ src/api.v1/Data/Context.Migrations/Migrations/20221103123053_InitialCreate.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Context.Migrations.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarListings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MaKe = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    UserId1 = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarListings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CarListings_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_CarListings_Users_UserId1",
+                        column: x => x.UserId1,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "CarListings",
+                columns: new[] { "Id", "MaKe", "Model", "UserId", "UserId1", "Year" },
+                values: new object[] { new Guid("71cd613d-4df8-4e07-8ca2-7f8277469adc"), "Toyota", "Camry", null, null, 2022 });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "DisplayName", "Email", "FirstName", "LastName" },
+                values: new object[] { new Guid("a2f3760c-a1b5-4134-9034-71d4e81eac17"), "Admin", "admin@carrentalcorporation.com", "John", "Doe" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "DisplayName", "Email", "FirstName", "LastName" },
+                values: new object[] { new Guid("dafce6d2-1dda-404d-8208-62751878227f"), "Developer", "janedoe@carrentalcorporation.com", "Jane", "Doe" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarListings_UserId",
+                table: "CarListings",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarListings_UserId1",
+                table: "CarListings",
+                column: "UserId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarListings");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
