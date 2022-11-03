985b4b2b046ec92d6d6d148996bdb5eca2661eb0
notauserx <notauserx@users.noreply.github.com>
2022-11-03 19:25:55 +0600

    add usercontroller

---
+++ src/api.v1/Monolith/CarRentals.Api/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentals.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly CarRentalsContext carRentalsContext;
+
+        public UsersController(CarRentalsContext carRentalsContext)
+        {
+            this.carRentalsContext = carRentalsContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return Ok(carRentalsContext.Users.ToList());
+        }
+    }
+}
