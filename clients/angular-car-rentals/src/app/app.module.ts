2ae958c606b0950f1a557d6bd2932d52386a01c5
notauserx <notauserx@users.noreply.github.com>
2022-11-07 08:59:37 +0600

    wip: add layout components

--- clients/angular-car-rentals/src/app/app.module.ts
+++ clients/angular-car-rentals/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MasterComponent } from './layout/master/master.component';
+import { TopnavComponent } from './layout/topnav/topnav.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterComponent,
+    TopnavComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
