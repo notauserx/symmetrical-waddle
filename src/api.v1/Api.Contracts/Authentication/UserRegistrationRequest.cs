3a4e6b924a6e11f45c693908c7870cd160326d00
notauserx <notauserx@users.noreply.github.com>
2022-11-13 23:11:28 +0600

    add user registration request and response contracts

--- src/api.v1/Api.Contracts/Authentication/UserRegistrationRequest.cs
+++ src/api.v1/Api.Contracts/Authentication/UserRegistrationRequest.cs
@@ -2,5 +2,8 @@
 
 public record UserRegistrationRequest
 {
-
+    public string? FirstNmae { get; set; }
+    public string? LastNmae { get; set; } 
+    public string? Email { get; set; }
+    public string? Password { get; set; }
 }
---
+++ src/api.v1/Api.Contracts/Authentication/UserRegistrationResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRentals.Api.Models;
+
+public record UserRegistrationResponse
+{
+    public bool IsSuccess { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string[]? ErrorMessages { get; set; }
+}
